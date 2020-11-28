15362725cdebbd7620e6d3ee6cff4b78654d2297
alexanderlehr <marakanerlp@gmail.com>
2020-11-28 17:56:15 +0100

    update

--- src/main/java/net/marakaner/varo/commands/TeamCommand.java
+++ src/main/java/net/marakaner/varo/commands/TeamCommand.java
@@ -1,4 +1,31 @@
 package net.marakaner.varo.commands;
 
-public class TeamCommand {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class TeamCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+
+        /**
+         * /team create [Name] [First] [Second] [Char]
+         * /team delete [Name]
+         * /team list
+         * /team info [Name]
+         *
+         */
+
+        if(args.length == 0) {
+
+        } else if(args.length == 1) {
+            if(args[0].equalsIgnoreCase("list")) {
+
+            }
+        } else if(args.length == 5) {
+
+        }
+
+        return true;
+    }
 }
